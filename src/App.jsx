b949a928eeba8a0ee0fc753c56f3529c1cf6a50e
Gökhan Tanrıverdi <codeodd3@gmail.com>
2022-10-12 19:44:53 +0300

    2. About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
+import AboutUs from "./pages/AboutUs"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Hello World</h1>
+    <AboutUs />
   )
 }
 
