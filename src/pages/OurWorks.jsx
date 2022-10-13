c1e16861b33fc8883402e1e3f455301380b3e0b0
Gökhan Tanrıverdi <codeodd3@gmail.com>
2022-10-13 10:56:45 +0300

    10. Our Works

--- src/pages/OurWorks.jsx
+++ src/pages/OurWorks.jsx
@@ -1,9 +1,60 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
 const OurWorks = () => {
     return (
-        <section>
-            <h1 className="py-10 text-white">Our Works</h1>
-        </section>
+        <Work>
+            <Movie>
+                <h2>The Athlete</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={athlete} alt="athlete" />
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>The Racer</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={theracer} alt="theracer" />
+                </Link>
+            </Movie>
+            <Movie>
+                <h2>Good Times</h2>
+                <div className="line"></div>
+                <Link>
+                    <img src={goodtimes} alt="goodtimes" />
+                </Link>
+            </Movie>
+        </Work>
     )
 }
+
+const Work = styled.div`
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 5rem 10rem;
+    //background: white;
+    h2 {
+        padding: 1rem 0;
+    }
+`;
+
+const Movie = styled.div`
+    padding-bottom: 10rem;
+    .line {
+        height: 0.5rem;
+        background: #ccc;
+        margin-bottom: 3rem;
+    }
+    img {
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+`;
 
 export default OurWorks;
