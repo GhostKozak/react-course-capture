1e1d0038b1343089be6cc25963f12ce0221c6fbb
Gökhan Tanrıverdi <codeodd3@gmail.com>
2022-10-15 12:17:17 +0300

    12. Framer Motion

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,22 +1,32 @@
-import styled from "styled-components";
 import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+    const titleAnimation = {
+        hidden: { opacity: 0 },
+        show: { opacity: 1, transition: { duration: 2 } },
+    }
+
+    const container = {
+        hidden: { x: 100 },
+        show: { x: 0, transition: { duration: 0.75, ease: "easeOut", staggerChildren: 0.75, when: "afterChildren" } }
+    }
+
     return (
         <About>
             <Description className="description">
-                <div className="title">
+                <motion.div className="title" variants={container} initial="hidden" animate="show"  >
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnimation} >We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnimation} >your <span>dreams</span> come</motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true</h2>
+                        <motion.h2 variants={titleAnimation} >true</motion.h2>
                     </Hide>
-                </div>
+                </motion.div>
                 <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
                 <button>Contact Us</button>
             </Description>
