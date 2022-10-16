3ec35b29534d96e5e0b22f732e3576a5d53930d4
Gökhan Tanrıverdi <codeodd3@gmail.com>
2022-10-16 14:45:36 +0300

    17. Reusable Components

--- src/components/FaqSection.jsx
+++ src/components/FaqSection.jsx
@@ -1,42 +1,52 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
+import Toggle from "./Toggle";
 
 const FaqSection = () => {
     return (
         <Faq>
             <h2>Any Questions? <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+            <Toggle>
+                <div className="question">
+                    <h4>How Do I Start</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+                    </div>
+                    <div className="faq-line"></div>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+            </Toggle>
+            <Toggle>
+                <div className="question">
+                    <h4>Daily Schedule</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+                    </div>
+                    <div className="faq-line"></div>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+            </Toggle>
+            <Toggle>
+                <div className="question">
+                    <h4>Different Payment Methods</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+                    </div>
+                    <div className="faq-line"></div>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What Products do you offer.</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+            </Toggle>
+            <Toggle>
+                <div className="question">
+                    <h4>What Products do you offer.</h4>
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto itaque temporibus recusandae labore autem adipisci quasi enim, possimus officia illo?</p>
+                    </div>
+                    <div className="faq-line"></div>
                 </div>
-                <div className="faq-line"></div>
-            </div>
+            </Toggle>
         </Faq>
     )
 }
